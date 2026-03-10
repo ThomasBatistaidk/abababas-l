7d6cab29f93a6c4488612ef10132d0da5e9e380e
ThomasBatistaidk <amanda.batista.silva23@escola.pr.gov.br>
2026-03-10 10:33:51 -0300

    Delete teste directory

--- teste/script.js
+++
@@ -1,7 +0,0 @@
-
-function salvar() {
-    let nome = document.getElementById("nome").value;
-    let idade = document.getElementById("idade").value;
-    console.log("Nome:", nome);
-    console.log("Idade:", idade);
-};
